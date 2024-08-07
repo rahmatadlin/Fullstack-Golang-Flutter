49c8715bce0cf4195f5f45431ad9321f0ce5a113
rahmatadlin <rahmatadlin210@gmail.com>
2024-08-08 06:08:49 +0700

    fix error and env

--- server/config/database.go
+++ server/config/database.go
@@ -1,50 +1,57 @@
 package config
 
 import (
-	"fmt"
-	"server/models"
+    "fmt"
+    "log"
+    "os"
+    "server/models"
 
-	"golang.org/x/crypto/bcrypt"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+    "github.com/joho/godotenv"
+    "golang.org/x/crypto/bcrypt"
+    "gorm.io/driver/mysql"
+    "gorm.io/gorm"
 )
 
-const (
-	host     = "localhost"
-	port     = 3306
-	user     = "root"
-	password = ""
-	dbName   = "tusk"
-)
+func init() {
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatalf("Error loading .env file")
+    }
+}
 
 func DatabaseConnection() *gorm.DB {
-	dsn := fmt.Sprintf(
-		"%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local",
-		user, password, host, port, dbName,
-	)
+    host := os.Getenv("DB_HOST")
+    port := os.Getenv("DB_PORT")
+    user := os.Getenv("DB_USER")
+    password := os.Getenv("DB_PASSWORD")
+    dbName := os.Getenv("DB_NAME")
 
-	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic(err)
-	}
+    dsn := fmt.Sprintf(
+        "%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
+        user, password, host, port, dbName,
+    )
 
-	return database
+    database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+    if err != nil {
+        panic(err)
+    }
+
+    return database
 }
 
 func CreateOwnerAccount(db *gorm.DB) {
-	hashedPasswordBytes, _ := bcrypt.GenerateFromPassword([]byte("123456"), bcrypt.DefaultCost)
-	owner := models.User{
-		Role:     "Admin",
-		Name:     "Owner",
-		Password: string(hashedPasswordBytes),
-		Email:    "owner@go.id",
-	}
+    hashedPasswordBytes, _ := bcrypt.GenerateFromPassword([]byte("123456"), bcrypt.DefaultCost)
+    owner := models.User{
+        Role:     "Admin",
+        Name:     "Owner",
+        Password: string(hashedPasswordBytes),
+        Email:    "owner@go.id",
+    }
 
-	// Condition if email already exist
-	if db.Where("email=?", owner.Email).First(&owner).RowsAffected == 0 {
-		db.Create(&owner)
-	} else {
-		fmt.Println("owner exist")
-	}
-
+    // Condition if email already exists
+    if db.Where("email=?", owner.Email).First(&owner).RowsAffected == 0 {
+        db.Create(&owner)
+    } else {
+        fmt.Println("Owner exists")
+    }
 }
